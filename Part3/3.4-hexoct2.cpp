f96887fb035f6fb37b1ea52185973390f08953fb
zhouhongyi <zhouhongyi1225@gmail.com>
2021-05-29 19:03:25 +0800

    add

---
+++ Part3/3.4-hexoct2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int mian()
+{
+        int chest =42;
+        int waist = 42;
+        int inseam = 42;
+
+        cout << "Monsietr cuts a striking figure!"  << endl;
+        cout << "chest =" << chest << "(decimal for 42)\n";
+        cout << hex;
+        cout << "waist = " << waist << "(hexadecimal for 42)\n";
+        cout << oct;
+        cout << "inseam = "<< inseam <<" (octal for 42)\n";
+
+        return 0;
+}
