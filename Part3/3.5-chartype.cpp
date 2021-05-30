2a729b36530a9a09df7819300ced77b1e2ea73de
zhouhongyi <zhouhongyi1225@gmail.com>
2021-05-30 21:42:51 +0800

    add

--- Part3/3.5-chartype.cpp
+++ Part3/3.5-chartype.cpp
@@ -8,7 +8,8 @@ int main()
         cout <<"Enter a character: \n";
         cin >> ch;              //gets only one character ,not a string;
         cout << "Hola! ";
-        cout << "Thank you for the " << ch << "character.\n";
+        cout << "Thank you for the " << (int)ch << " character.\n";             //(int)Type conversion;
+        cout << '$' << endl;
 
         return 0;
 }
