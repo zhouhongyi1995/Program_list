6fdef6098d42b8aed4b6f2f0156800e9bf7580ab
zhouhongyi1225 <zhouhongyi1225@gmail.com>
2021-09-08 17:14:44 +0800

    add

---
+++ C++_Primer_code/part5/5.12-compstr2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+        string word = "?ate";
+        for (char ch = 'a'; word != "mate"; ch++)
+        {
+                cout << word << endl;
+                word[0] = ch;
+        }
+        cout << "After loop ends, word is " << word << endl;
+
+        return 0;
+}
