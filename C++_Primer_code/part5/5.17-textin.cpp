c6003225b531dd9a19fc76b23e0dff0fb6599589
zhouhongyi1995 <zhouhongyi1225@gmail.com>
2021-09-13 23:13:44 +0800

    add

---
+++ C++_Primer_code/part5/5.17-textin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int count = 0;
+
+    cout << "Enter charachers; enter # to quit:\n";
+    cin.get(ch);
+    while (ch != '#')
+    {
+        cout << ch;
+        ++count;
+        cin.get(ch);
+    }
+    cout << endl
+         << count << " characters read\n";
+
+    return 0;
+}
