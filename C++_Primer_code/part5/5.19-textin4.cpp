8c58d03fd8756e78a0a04950f5215e829ceeda3b
zhouhongyi1995 <zhouhongyi1225@gmail.com>
2021-09-16 22:23:13 +0800

    add

---
+++ C++_Primer_code/part5/5.19-textin4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ch;
+    int count = 0;
+
+    while ((ch = cin.get() != EOF))
+    {
+        cout.put(char(ch));
+        ++count;
+    }
+    cout << endl
+         << count << " characters read\n";
+    return 0;
+}
