d64e03cbe203835d9b864101990f3049e8416fd6
zhouhongyi1995 <zhouhongyi1225@gmail.com>
2021-09-21 22:11:15 +0800

    add

---
+++ C++_Primer_code/part6/6.5-and.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+const int ArSize = 6;
+
+int main()
+{
+    float naaq[ArSize];
+    cout << "Enter the NAAQs (New Age Awareness Quotients) of \nyour neighbors."
+            " Program termianties when you manke\n"
+         << ArSize << " entries or enter a negative value.\n";
+    int i = 0;
+    float temp;
+    cout << "First value: ";
+    cin >> temp;
+    while (i < ArSize && temp >= 0)
+    {
+        naaq[i] = temp;
+        ++i;
+        if (i < ArSize)
+        {
+            cout << "Next value: ";
+            cin >> temp;
+        }
+    }
+    if (i == 0)
+    {
+        cout << "No data--bye\n";
+    }
+    else
+    {
+        cout << "Enter your NAAQ: ";
+        float you;
+        cin >> you;
+        int count = 0;
+        for (size_t j = 0; j < i; j++)
+        {
+            if (naaq[j] > you)
+            {
+                ++count;
+            }
+        }
+        cout << count;
+        cout << " fo your neighbors have greater awareness of\n"
+             << "the New Age than you do.\n";
+    }
+    return 0;
+}
