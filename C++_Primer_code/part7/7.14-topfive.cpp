5dbf2312f2b7e7c63b28937491a7eeeed2b44c5f
zhouhongyi1995 <zhouhongyi1225@gmail.com>
2021-12-09 08:46:10 +0800

    updata

--- C++_Primer_code/part7/7.14-topfive.cpp
+++ C++_Primer_code/part7/7.14-topfive.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 #include <string>
+using namespace std;
+const int SIZE = 5;
+void dispaly(const string sa[], int n);
+
+int main()
+{
+    string list[SIZE];
+    cout << "Enter your " << SIZE << " favorite astronomical sights:\n";
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        cout << i + 1 << ": ";
+        getline(cin, list[i]);
+    }
+    cout << "Your list:\n";
+    dispaly(list, SIZE);
+
+    return 0;
+}
+
+void dispaly(const string sa[], int n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cout << i + 1 << ": " << sa[i] << endl;
+    }
+}
