7329a49c96fe58492763399e7f37bb4dd1a2c242
zhouhongyi1995 <zhouhongyi1225@gmail.com>
2023-06-30 16:27:47 +0800

    add

--- Python_Practice_Code/Python编程：从入门到实践/第3章：列表/friends.py
+++ Python_Practice_Code/Python编程：从入门到实践/第3章：列表/friends.py
@@ -1,3 +1,12 @@
-Friends_names = ['hesong', 'yangzhe','lichenggang','wuxiujing','lilei','chenrui',]
+Friends_names = ['he song', 'yang zhe','li chenggang','wu xiujing','li lei','chen rui',]
+Friends_names.append('zhou hongyi')
+Friends_names.insert(1,'zhang hao')
 for name in Friends_names:
-    print(name)
+    print(name.title())
+Friends_names[0]='wen yuxuan'
+print(Friends_names[0].title())
+
+popped_friend= Friends_names.pop()
+print(Friends_names)
+print(popped_friend)
+
---
+++ Python_Practice_Code/Python编程：从入门到实践/第3章：列表/guests.py
@@ -0,0 +1,2 @@
+guests_name=['yang zhe','chen rui','li lei',]
+print('I expect to invite ' + guests_name[0].title() +',' +guests_name[1].title() + ',' + guests_name[2].title() + ' to the dinner.')
